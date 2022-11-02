f050963183827ea4fac3671c4f571c0d30e1a3d1
Jose <janc18@github.com>
2022-11-02 13:00:13 -0600

    Sanitización de código

--- Código_ESP_IDF/main/main.c
+++ Código_ESP_IDF/main/main.c
@@ -1,27 +1,16 @@
-
 #include "freertos/FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "freertos/task.h"
 #include "esp_attr.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
-#include "ConfADC.h"
-#include "ConfMCPWM.h"
-#include "ControladorMotor.h"
-#include "ControladorI2c.h"
-#include "ConfI2c.h" 
 #include "freertos/queue.h"
-#include "Usb.h"
+
+#include "Encoders.h"
 
 
-
-
-//int *ptr_VoltajeRaw=NULL;
 void app_main() {
-   IniciarUSbUART();
-   //transmitir("la cica");
-   transmitir();
-   transmitirint(70);
+
 
 
 }

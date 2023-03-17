9b5cf1aca6c13717d74c103525b5734a54f8d06b
Jose <josecla011@gmail.com>
2023-03-17 17:00:57 -0600

    added commands to do the spi communication

--- Codigo/Firmware/Main_board_HID/ESP32-S3/main/app_main.c
+++ Codigo/Firmware/Main_board_HID/ESP32-S3/main/app_main.c
@@ -6,23 +6,22 @@
     software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
     CONDITIONS OF ANY KIND, either express or implied.
 */
-#include "esp_log.h"
 #include "Spi_communication.h"
-#define PIN_NUM_MISO 37 
+#include "esp_log.h"
+#define PIN_NUM_MISO 37
 #define PIN_NUM_MOSI 35
-#define PIN_NUM_CLK  36
-#define PIN_NUM_CS   45
-void app_main()
-{
-uint16_t data=0x1234;
-    //uint8_t data[4]={0x10,0x11,0x12,0x13};
-    spi_device_handle_t spi=init_spi_device();
-    //Send_Spi_Transaction(data,spi);
-	send_spi_data(spi,data);
+#define PIN_NUM_CLK 36
+#define PIN_NUM_CS 45
+void app_main() {
+  uint16_t data = 0x1234;
+  // uint8_t data[4]={0x10,0x11,0x12,0x13};
+  spi_device_handle_t spi = init_spi_device();
+  // Send_Spi_Transaction(data,spi);
+  send_spi_data(spi, data);
 
-    // Remove the device from the SPI bus
-    //spi_bus_remove_device(spi);
+  // Remove the device from the SPI bus
+  // spi_bus_remove_device(spi);
 
-    // Free the SPI bus
-    //spi_bus_free(SPI2_HOST);
+  // Free the SPI bus
+  // spi_bus_free(SPI2_HOST);
 }
